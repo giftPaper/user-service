d33ac7cadd71dd37c1d829ca4c53354efbcddbf8
Рустам Таов <RSTaov@sberbank.ru>
2023-03-11 02:44:15 +0300

    DREAM-7: fix pom.xml, fix application.yml delete liquibase.properties

--- src/main/java/user/UserServiceApplication.java
+++ src/main/java/user/UserServiceApplication.java
@@ -1,9 +1,14 @@
 package user;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User-service"))
 @EnableJpaAuditing
 @SpringBootApplication
 public class UserServiceApplication {
